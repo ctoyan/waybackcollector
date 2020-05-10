a3ad6266e8114e805aea8bdd1a90518d84ab4c67
STOYAN KIROV <kirov@STOYANs-MBP.lan>
2020-05-11 01:25:35 +0300

    Refactor

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"fmt"
-	"net/http"
-	"encoding/json"
-	"io/ioutil"
 	"crypto/sha1"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
 type HistoryItem struct {
 	Timestamp string
-	Digest 		string
-	Length 		string
+	Digest    string
+	Length    string
 }
 
 func main() {
-	url 		   := flag.String("url", "", "URL pattern to collect responses for")
-	dateFrom   := flag.String("from", "", "Date on which to start collecing responses. Inclusive. Format: yyyyMMddhhmmss. Defaults to first ever record.")
-	dateTo 	   := flag.String("to", "", "Date on which to end collecing responses. Inclusive. Format: yyyyMMddhhmmss. Defaults to last ever record.")
-	limit 	   := flag.Int("limit", 0, "Limit the results")
-	filter 	 	 := flag.String("filter", "", "Filter your search, using the wayback cdx filters (find more here: https://github.com/internetarchive/wayback/tree/master/wayback-cdx-server#filtering)")
+	url := flag.String("url", "", "URL pattern to collect responses for")
+	dateFrom := flag.String("from", "", "Date on which to start collecing responses. Inclusive. Format: yyyyMMddhhmmss. Defaults to first ever record.")
+	dateTo := flag.String("to", "", "Date on which to end collecing responses. Inclusive. Format: yyyyMMddhhmmss. Defaults to last ever record.")
+	limit := flag.Int("limit", 0, "Limit the results")
+	filter := flag.String("filter", "", "Filter your search, using the wayback cdx filters (find more here: https://github.com/internetarchive/wayback/tree/master/wayback-cdx-server#filtering)")
 
-	urls		 	 := flag.Bool("urls", false, "Print to stdout only a list of historic URLs, which you can request later")
-	unique		 := flag.Bool("unique", false, "Print to stdout only unique reponses")
-	output 		 := flag.String("output", "", "Path to a folder where the tool will safe all unique responses in uniquely named files per response (meg style output)")
+	urls := flag.Bool("urls", false, "Print to stdout only a list of historic URLs, which you can request later")
+	unique := flag.Bool("unique", false, "Print to stdout only unique reponses")
+	output := flag.String("output", "", "Path to a folder where the tool will safe all unique responses in uniquely named files per response (meg style output)")
 
 	flag.Parse()
 
@@ -34,19 +34,27 @@ func main() {
 		log.Fatal("url argument is required")
 	}
 
-	if (*urls && *unique)     ||
-		 (*urls && *output != "")   ||
-		 (*unique && *output != "") {
+	if (*urls && *unique) ||
+		(*urls && *output != "") ||
+		(*unique && *output != "") {
 		log.Fatal("you can only set one of the following arguments: urls, unique, output")
 	}
 
 	requestUrl := fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%v&output=json&fl=timestamp,digest,length", *url)
 
-	if *dateFrom != ""  { requestUrl += fmt.Sprintf("&from=%v", *dateFrom) }
-	if *dateTo 	 != ""  { requestUrl += fmt.Sprintf("&to=%v", *dateTo) }
-	if *limit 	 != 0 	{ requestUrl += fmt.Sprintf("&limit=%v", *limit) }
-	if *filter 	 != ""  { requestUrl += fmt.Sprintf("&filter=%v", *filter) }
-	
+	if *dateFrom != "" {
+		requestUrl += fmt.Sprintf("&from=%v", *dateFrom)
+	}
+	if *dateTo != "" {
+		requestUrl += fmt.Sprintf("&to=%v", *dateTo)
+	}
+	if *limit != 0 {
+		requestUrl += fmt.Sprintf("&limit=%v", *limit)
+	}
+	if *filter != "" {
+		requestUrl += fmt.Sprintf("&filter=%v", *filter)
+	}
+
 	uniqueResponses := make(map[[20]byte][]byte)
 	var allHistoryUrls []string
 
@@ -103,14 +111,14 @@ func get(url string) []byte {
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
+	if err != nil {
 		log.Fatalf("error reading response body: %v", err)
-  }
+	}
 
 	return body
 }
 
-func getHistoryItems (requestUrl string) []HistoryItem {
+func getHistoryItems(requestUrl string) []HistoryItem {
 	body := get(requestUrl)
 
 	var timestamps2d [][]string
@@ -121,11 +129,13 @@ func getHistoryItems (requestUrl string) []HistoryItem {
 
 	var timestamps []HistoryItem
 	for i, val := range timestamps2d {
-		if i == 0 { continue }
-		timestamps = append(timestamps, HistoryItem {
+		if i == 0 {
+			continue
+		}
+		timestamps = append(timestamps, HistoryItem{
 			Timestamp: val[0],
-			Digest: 	 val[1],
-			Length:		 val[2],
+			Digest:    val[1],
+			Length:    val[2],
 		})
 	}
 	return timestamps
